cc263c80b4e90bb5826672a52ef147f137397079
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-02-10 11:14:29 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import streamlit as st
-
 def main():
-    st.title("Login Page")
+    st.title("User Management Page")
 
     username = st.text_input("Username")
     password = st.text_input("Password", type='password')
@@ -11,6 +10,22 @@ def main():
             st.success("Login successful")
         else:
             st.error("Incorrect login or password")
+    
+    if st.checkbox("Reset username"):
+        new_username = st.text_input("Enter new username")
+        if st.button("Update"):
+            username = new_username
+            st.success("Username updated successfully")
+    
+    if st.checkbox("Reset password"):
+        new_password = st.text_input("Enter new password", type='password')
+        confirm_password = st.text_input("Confirm new password", type='password')
+        if st.button("Update"):
+            if new_password == confirm_password:
+                password = new_password
+                st.success("Password updated successfully")
+            else:
+                st.error("Password and confirmation do not match")
 
 if __name__ == '__main__':
     main()
