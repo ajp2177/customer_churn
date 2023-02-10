f001549f804d86f1c0b7260446d678a722286b09
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-02-10 11:15:52 -0800

    Update test.py

--- test.py
+++ test.py
@@ -13,14 +13,14 @@ def main():
     
     if st.checkbox("Reset username"):
         new_username = st.text_input("Enter new username")
-        if st.button("Update"):
+        if st.button("Update username"):
             username = new_username
             st.success("Username updated successfully")
     
     if st.checkbox("Reset password"):
         new_password = st.text_input("Enter new password", type='password')
         confirm_password = st.text_input("Confirm new password", type='password')
-        if st.button("Update"):
+        if st.button("Update password"):
             if new_password == confirm_password:
                 password = new_password
                 st.success("Password updated successfully")
