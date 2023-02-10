a3107e298d1527bb755c529fc7b6aa985fd15ba8
Andrew P <118642994+ajp2177@users.noreply.github.com>
2023-02-10 11:11:00 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 import streamlit as st
-st.markdown("Hello")
-st.sidebar.button("help")
+
+def main():
+    st.title("Login Page")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type='password')
+
+    if st.button("Submit"):
+        if username == "admin" and password == "password":
+            st.success("Login successful")
+        else:
+            st.error("Incorrect login or password")
+
+if __name__ == '__main__':
+    main()
